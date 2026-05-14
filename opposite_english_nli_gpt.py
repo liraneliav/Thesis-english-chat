53be8db45d1a5313d0f2446e6dd29adac9d70e82
Liran Eliav <102174057+liraneliav@users.noreply.github.com>
2026-05-14 18:59:44 +0300

    change of API version

--- opposite_english_nli_gpt.py
+++ opposite_english_nli_gpt.py
@@ -44,15 +44,15 @@ else:
 endpoint = os.getenv("ENDPOINT_URL")
 subscription_key = os.getenv("AZURE_OPENAI_API_KEY")
 
-# client = AzureOpenAI(
-#     azure_endpoint=endpoint,
-#     api_key=subscription_key,
-#     api_version="2025-01-01-preview",
-# )
-
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
+client = AzureOpenAI(
+    azure_endpoint=endpoint,
+    api_key=subscription_key,
+    api_version="2024-12-01-preview",
 )
+
+# client = OpenAI(
+#     api_key=os.getenv("OPENAI_API_KEY")
+# )
 
 import requests
 
