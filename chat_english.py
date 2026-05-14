92a48a13e8b789f68608ab7452fc04a90a8d5076
Liran Eliav <102174057+liraneliav@users.noreply.github.com>
2026-05-14 18:58:56 +0300

    change of API version

--- chat_english.py
+++ chat_english.py
@@ -27,7 +27,7 @@ subscription_key = os.getenv("AZURE_OPENAI_API_KEY")
 client = AzureOpenAI(
     azure_endpoint=endpoint,
     api_key=subscription_key,
-    api_version="2025-01-01-preview",
+    api_version="2024-12-01-preview",
 )
 
 # @st.cache_resource(show_spinner=False)
